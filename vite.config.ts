09ffed778e9a20e658dd5d93ba4121e94cffe772
Felipepmerj <167003456+Felipepmerj@users.noreply.github.com>
2025-05-12 11:30:29 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,31 +3,16 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-  ],
+export default defineConfig({
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-    },
-    dedupe: ['react-image-crop']
-  },
-  optimizeDeps: {
-    include: ['react-image-crop']
+    }
   },
   build: {
-    rollupOptions: {
-      external: ['react-image-crop'],
-      output: {
-        globals: {
-          'react-image-crop': 'ReactCrop'
-        }
-      }
+    commonjsOptions: {
+      include: [/node_modules/]
     }
   }
-}));
+});
